157dfde55c4d6c5a19776946741c56e85816a078
SayanSahu99 <sayansovan@gmail.com>
2021-04-12 18:33:23 +0000

    convert file to jpg

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,29 @@
 from app import app
-from flask import render_template, request, flash, redirect;
-
+from flask import render_template, request, flash, send_file, redirect
+from app.convert import convert
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template("index.html");
+  print(os.getcwd())
+  print()
+  return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def upload_file():
   if request.method == 'POST':
 
-      if 'files[]' not in request.files:
-          flash('No file part')
-          return redirect(request.url)
+    if 'files[]' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-      files = request.files.getlist('files[]')
+    files = request.files.getlist('files[]')
 
-      flash('File(s) successfully uploaded')
-      print(files)
-      return redirect('/')
+    for file in files:
+      if file.filename != '':
+        file.save(os.path.join(os.path.join(os.path.realpath(__package__), "uploads"), file.filename))
+        print(file.filename)
+
+    convert(files)
+    return send_file(os.path.join(os.getcwd(), "sample.zip"), as_attachment=True)
