717cd28851376d5b9b1c8fee82b7698554de7807
SayanSahu99 <sayansovan@gmail.com>
2021-04-12 11:25:15 +0000

    Adding Post route

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,22 @@
 from app import app
-from flask import render_template;
+from flask import render_template, request, flash, redirect;
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html");
+  return render_template("index.html");
 
+@app.route('/', methods=['POST'])
+def upload_file():
+  if request.method == 'POST':
 
+      if 'files[]' not in request.files:
+          flash('No file part')
+          return redirect(request.url)
+
+      files = request.files.getlist('files[]')
+
+      flash('File(s) successfully uploaded')
+      print(files)
+      return redirect('/')
