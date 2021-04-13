73e0f1ab0d605fadf1f3cd436a79d6ee357effb6
SayanSahu99 <sayansovan@gmail.com>
2021-04-13 05:52:23 +0000

    Using config's upload folder path

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,18 @@
 from app import app
 from flask import render_template, request, flash, send_file, redirect
-from app.convert import convert
+from app.convert import convert_jpg, convert_png
 import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-  print(os.getcwd())
-  print()
+  if(os.path.exists(os.path.join(os.getcwd(), 'sample.zip'))):
+    os.remove(os.path.join(os.getcwd(), 'sample.zip'))
   return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def upload_file():
   if request.method == 'POST':
-
     if 'files[]' not in request.files:
         flash('No file part')
         return redirect(request.url)
@@ -22,8 +21,13 @@ def upload_file():
 
     for file in files:
       if file.filename != '':
-        file.save(os.path.join(os.path.join(os.path.realpath(__package__), "uploads"), file.filename))
+        file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename))
         print(file.filename)
 
-    convert(files)
+    extension = 'jpg'
+    data = request.form.get('ext', extension)
+    if data == 'jpg':
+      convert_jpg(files)
+    else:
+      convert_png(files)
     return send_file(os.path.join(os.getcwd(), "sample.zip"), as_attachment=True)
